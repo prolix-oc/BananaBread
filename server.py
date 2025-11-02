010214e66a85f573a60be32de22a9b633354ee77
Darran Hall <me@prolix.live>
2025-11-02 02:45:44 -0500

    Push server changes

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 import os
+
+# Set PyTorch CUDA memory optimization (before torch import)
+# Reduces VRAM fragmentation and improves memory efficiency
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True,max_split_size_mb:128'
+
 import json
 import secrets
 import hashlib
